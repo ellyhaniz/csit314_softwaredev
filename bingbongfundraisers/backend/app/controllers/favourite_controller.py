c2117b0ad4f4adc1c15d7b181526b36a60dedbb9
patrickheng18-star <patrickheng18@gmail.com>
2026-05-09 16:09:31 +0800

    Update favourite_controller.py

--- bingbongfundraisers/backend/app/controllers/favourite_controller.py
+++ bingbongfundraisers/backend/app/controllers/favourite_controller.py
@@ -4,7 +4,7 @@ class FavouritesController:
     @staticmethod
     def save_fra(user_id: int, fra_id: int):
         return FavouriteService.save_fra(user_id, fra_id)
-//-----------
+
     @staticmethod
     def get_favourites_list(user_id: int):
         return FavouriteService.get_favourites(user_id)
